832fd837017386aa1189c78a84872db8bc209bf8
Minwei Xu <faceswilliam@gmail.com>
2015-12-28 01:26:48 +0800

    router

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var ListManager = require('./components/ListManager.jsx');
+var Routes = require('./Routes.jsx');
+// var ListManager = require('./components/ListManager.jsx');
 
-ReactDOM.render(< ListManager title = 'Ingredients' />, document.getElementById('ingredients'));
-ReactDOM.render(< ListManager title = 'ToDo' />, document.getElementById('todo'));
-ReactDOM.render(< ListManager title = 'Christmas' headingColor="#b31217" />, document.getElementById('christmas'));
+// ReactDOM.render(< ListManager title = 'Ingredients' />, document.getElementById('ingredients'));
+// ReactDOM.render(< ListManager title = 'ToDo' />, document.getElementById('todo'));
+// ReactDOM.render(< ListManager title = 'Christmas' headingColor="#b31217" />, document.getElementById('christmas'));
+
+ReactDOM.render(Routes, document.getElementById('ingredients'));
